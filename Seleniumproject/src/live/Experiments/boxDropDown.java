e088daa2790b9cb0982df8df3e127282992f210d
ameersuahiltk <disneywelcome786@gmail.com>
2026-07-07 14:53:05 +0300

    added Drop down box

---
+++ Seleniumproject/src/live/Experiments/boxDropDown.java
@@ -0,0 +1,34 @@
+package live.Experiments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class boxDropDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		
+		WebElement dropdown = driver.findElement(By.tagName("select"));
+		dropdown.click();
+	
+		Select slt = new Select(dropdown);
+//		slt.selectByIndex(8); //By indexing
+//		slt.selectByValue("ARG"); //By Value
+		slt.selectByVisibleText("Belgium");  //By Text
+		
+		driver.quit();
+
+	}
+
+}
